f4cde3692b45dace43df31b17ba65f72523ae898
wertiop1 <102436461+wertiop1@users.noreply.github.com>
2025-12-10 10:34:51 -0800

    dice rolls

---
+++ dice.py
@@ -0,0 +1,37 @@
+Die1 = [1, 2, 3, 4, 5, 6]
+Die2 = [1, 2, 3, 4, 5, 6]
+Prob = {
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0,
+    12: 0,
+}
+
+for d1 in Die1:
+    for d2 in Die2:
+        Prob[d1 + d2] += 1
+
+doubleProb = {
+    2: 0,
+    4: 0,
+    6: 0,
+    8: 0,
+    10: 0,
+    12: 0,
+}
+
+for key in Prob:
+    if key % 2 == 0:
+        Prob[key] -= 1
+        doubleProb[key] += 1
+print("Prob: ", end="")
+print(Prob)
+print("Double Prob: ", end="")
+print(doubleProb)
