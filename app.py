be54e892a5a517c412a0ab3c02f7f766fb2c70db
wertiop1 <102436461+wertiop1@users.noreply.github.com>
2025-12-16 08:50:43 -0800

    more stuff

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, url_for
+from PIL import Image, ImageEnhance
 import numpy as np
+import os
 app = Flask(__name__)
 
+def reset():
+    pass
+
+def opacityById(ID, opacity):
+    pass
 
 @app.route("/")
 def monopolysim():
